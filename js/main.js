1e3912ed110390144dd72a1eb056a872765926f0
SecDet Samurai <tylerblackout17@gmail.com>
2021-06-05 15:22:15 +0300

    xbox site

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function () {
+    $('.slider').slick({
+        arrows: true,
+        fade: true,
+        autoplay: 3000,
+        dots: true
+    })
+
+    $('.slider2').slick({
+        arrows: true,
+        fade: true,
+        autoplay: 3000,
+        dots: true
+    })
+});
